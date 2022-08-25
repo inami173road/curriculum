221984d066ca26c49254f2b63240ad421c203d67
inami <inami173road@gmail.com>
2022-08-25 20:57:03 +0900

    Add 4-3

--- JavaStudy/4-3/sqlTask.sql
+++ JavaStudy/4-3/sqlTask.sql
@@ -19,15 +19,25 @@ FROM stock_table
 
 -- 4. 商品テーブルから全商品の単価の平均値を抽出しなさい。
 -- [回答]
+SELECT  FLOOR(AVG(price)) FROM goods_table;
 
 -- 5. 店舗コード='EA01'の在庫数の平均値より大きい在庫数を持つ店舗コードを抽出しなさい。
 -- [回答]
+SELECT store_code FROM stock_table WHERE quantity >(SELECT AVG(quantity) FROM stock_table WHERE store_code ='EA01');
 
 -- 6. 商品テーブルに「商品コード='M001'、商品名='マフラー'、単価=4500円、更新日付=本日日付」のデータを追加しなさい。※実行後のSELECT結果も貼付すること。
 -- [回答]
+INSERT 
+    INTO goods_table (goods_code,goods_name,price,update_day) 
+	VALUES ('M001','マフラー','4500',CURRENT_DATE);
+SELECT * FROM goods_table;
 
 -- 7. 在庫テーブルの商品コード='S987'、かつ、店舗コード='EA01'に対して、「在庫数=10、更新日付=本日日付」で更新しなさい。※実行後のSELECT結果も貼付すること。
 -- [回答]
+UPDATE stock_table SET quantity = '10',update_day = CURRENT_DATE WHERE goods_code = 'S987' AND store_code = 'EA01';
+SELECT * FROM stock_table;
 
 -- 8. 7で更新した商品を削除しなさい。※実行後のSELECT結果も貼付すること。
 -- [回答]
+DELETE FROM stock_table WHERE goods_code = 'S987' AND store_code = 'EA01';
+SELECT * FROM stock_table;
